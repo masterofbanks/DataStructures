40330e06d5ac99a36aa5183b03eb977cf08bcfb0
masterofbanks <tarikalex2003@gmail.com>
2024-12-28 18:57:53 -0500

    added a clear plus destructor methods
    should help with memory leaks but should download some extra checker from the web to see if it's working

--- main.cpp
+++ main.cpp
@@ -4,10 +4,20 @@
 int main(){
     Singly_Linked_List List;
     
-    for(unsigned int i = 0; i < 10; i++){
-        List.AddTail(i);
-        List.AddHead(i);
-    }
+    List.AddHead(0);
+    List.AddTail(2);
+    List.InsertAfter(1, 1);
+
+    List.Print();
+    std::cout << List.Size() << std::endl;
+
+    List.Clear();
+    
+    List.Print();
+    std::cout << List.Size() << std::endl;
+
+    List.AddHead(0);
+    
     List.Print();
     std::cout << List.Size() << std::endl;
     return 0;
